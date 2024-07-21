06eee2d0d6c663dff552095d1760f641df0c91c8
William <william@Williamui-iMac.local>
2024-07-21 23:37:53 +0900

    7/21 23:37 ~판다스 개요와 기본 API - 02

---
+++ PerfectGuide/1장/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+titanic_df = pd.read_csv('titanic_train.csv')
+titanic_df
--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-array1 = np.array([1,2,3,4])
-
-plt.hist(array1)
-plt.show()
